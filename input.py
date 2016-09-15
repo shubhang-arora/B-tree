638d5b885cb28171cb7889940a2ce8ff3b326a0f
shubhang-arora <sa287@snu.edu.in>
2016-09-15 20:23:35 +0530

    creating input file with random int

--- input.py
+++ input.py
@@ -1,3 +1,10 @@
 import os
+import random
 path = os.getcwd()
 print(path)
+helloFile = open(os.getcwd()+'/hello.txt','w')
+for i in range(1,200000):
+    helloFile.write(str(random.randint(1,20000)))
+    helloFile.write(",")
+
+helloFile.close()
