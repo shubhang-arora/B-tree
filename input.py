315c54992e963d0493970fa80b2cdcd57324586f
Sahil Sharma <sahilsharma011@users.noreply.github.com>
2016-09-15 23:26:55 +0530

    Update input.py

--- input.py
+++ input.py
@@ -3,16 +3,9 @@ import random
 
 
 def createfile(name, number):
-    path = os.getcwd()
-    inputFile = open(os.getcwd()+'/'+name+'.txt','w')
+    inputfile = open(os.getcwd() + '/' + name + '.txt', 'w')
     for i in range(1, int(number)):
-        inputFile.write(str(random.randint(100, 20000)))
-        inputFile.write(",")
-    inputFile.close()
-
-
-if __name__ == '__main__':
-
-    name = raw_input("Input name of the File you want to create")
-    number = raw_input("Enter the number of objects you want to write in the file")
-    createfile(name, number)
+        inputfile.write(str(random.randint(100, 20000)))
+        inputfile.write("\n")
+    inputfile.write(str(random.randint(100, 20000)))
+    inputfile.close()
