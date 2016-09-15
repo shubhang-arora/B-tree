53b0caea1d17efaf1f8519af89d84ca1eb60174a
shubhang-arora <sa287@snu.edu.in>
2016-09-15 21:03:14 +0530

    Getting details from user

--- input.py
+++ input.py
@@ -1,10 +1,18 @@
 import os
 import random
-path = os.getcwd()
-print(path)
-helloFile = open(os.getcwd()+'/hello.txt','w')
-for i in range(1,200000):
-    helloFile.write(str(random.randint(1,20000)))
-    helloFile.write(",")
 
-helloFile.close()
+
+def createfile(name, number):
+    path = os.getcwd()
+    inputFile = open(os.getcwd()+'/'+name+'.txt','w')
+    for i in range(1, int(number)):
+        inputFile.write(str(random.randint(100, 20000)))
+        inputFile.write(",")
+    inputFile.close()
+
+
+if __name__ == '__main__':
+
+    name = raw_input("Input name of the File you want to create")
+    number = raw_input("Enter the number of objects you want to write in the file")
+    createfile(name, number)
